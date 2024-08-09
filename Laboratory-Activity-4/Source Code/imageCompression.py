5a2bbddefb7f5bc2106e962b185bc174fe1671f6
Jared Randalle Bauyon <168247604+JaredBauyon@users.noreply.github.com>
2024-08-09 12:09:31 +0800

    Delete Laboratory-Activity-4 directory

--- Laboratory-Activity-4/Source Code/imageCompression.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-# Open an image file
-with Image.open('randImg1.jpg') as img:
-
-# Compress the image
-    img.save('compressedRandImg.jpg', quality = 85, optimize = True)
-
-img.show()
